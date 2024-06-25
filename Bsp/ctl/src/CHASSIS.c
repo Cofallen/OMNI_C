5e9db5fbb73f8650436f450e1b400fb1b6858e4f
Cofallen <1727334304@qq.com>
2024-06-25 22:15:20 +0800

    功率限制差电机控制

--- Bsp/ctl/src/CHASSIS.c
+++ Bsp/ctl/src/CHASSIS.c
@@ -9,6 +9,8 @@
 #include "PID.h"
 #include "TIM_DEV.h"
 
+#include "Read_Data.h"
+
 // 本代码为全向轮底盘运动基本代码
 
 void CHASSIS_F_Ctl(TYPEDEF_MOTOR *MOTOR, TYPEDEF_DBUS *DBUS)
@@ -48,4 +50,73 @@ void CHASSIS_F_Ctl(TYPEDEF_MOTOR *MOTOR, TYPEDEF_DBUS *DBUS)
     PID_F_SC(&MOTOR[MOTOR_D_CHASSIS_2]);
     PID_F_SC(&MOTOR[MOTOR_D_CHASSIS_3]);
     PID_F_SC(&MOTOR[MOTOR_D_CHASSIS_4]);
+}
+
+// The following code is for reference ZJU Power Control
+// Reference Site: https://zju-helloworld.github.io/Wiki/
+// fitting function :
+// P = \omega * M + P_{loss}
+//              M = k_{M} * i_{q}, consultation, k_{M} = 0.3 Nm/A
+//                  P_{loss} = R * i^2
+//                             R = p_{1} = 0.1249
+//                                 p_{2} = -0.007836 -> 0
+//                                 p_{3} = 5.952, circuit power
+// @TODO using karman filter -> k 
+// using function predict
+
+struct CHASSIS_F_PwrLimit
+{
+    float Pwr;
+    float Z;
+    float k;
+    float R;
+    float PwrSum;
+    float PwrPred[4];
+}CHASSIS_V_PwrLimit;
+
+
+void CHASSIS_F_PwrLimitInit()
+{
+
+}
+
+
+void CHASSIS_F_PwrLimitCalc(TYPEDEF_MOTOR *MOTOR)
+{
+    CHASSIS_V_PwrLimit.Pwr = user_data.power_heat_data.chassis_power;
+    CHASSIS_V_PwrLimit.Z   = user_data.power_heat_data.buffer_energy;
+
+    CHASSIS_V_PwrLimit.R = 0.3f;
+    CHASSIS_V_PwrLimit.k = 0.1249f;  // @TODO k need to calculate by karman filter
+    CHASSIS_V_PwrLimit.PwrSum = 0.0f;
+
+    // dimension transform
+    float current[4] = {0.0f};
+    for (int i = 0; i < 4; i++)
+    {
+        // dimension transform
+        current[i] = MOTOR[i].DATA.CURRENT * 0.001220703125f;
+        CHASSIS_V_PwrLimit.PwrPred[i] = CHASSIS_V_PwrLimit.k * current[i] * MOTOR[i].DATA.SPEED_NOW \
+                        + CHASSIS_V_PwrLimit.R * current[i] * current[i];
+        if (CHASSIS_V_PwrLimit.PwrPred)
+            continue;
+        CHASSIS_V_PwrLimit.PwrSum += CHASSIS_V_PwrLimit.PwrPred[i];
+    }
+    CHASSIS_V_PwrLimit.PwrSum += CHASSIS_V_PwrLimit.Pwr;
+
+    // @TODO no consider the cap
+    CHASSIS_V_PwrLimit.PwrSum -= CHASSIS_V_PwrLimit.Z;
+
+    if (CHASSIS_V_PwrLimit.PwrSum > CHASSIS_V_PwrLimit.Pwr)
+    {
+        float scale = CHASSIS_V_PwrLimit.Pwr / CHASSIS_V_PwrLimit.PwrSum;
+        for (int i = 0; i < 4; i++)
+        {
+            CHASSIS_V_PwrLimit.PwrPred[i] *= scale;
+        }
+    }
+
+    // @TODO limit the power to motor
+    
+    
 }
