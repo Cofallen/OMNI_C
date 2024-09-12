0a26ed9d8d499aa2da79c8424c5717f1493785da
Cofallen <1727334304@qq.com>
2024-09-12 20:43:11 +0800

    云台初始化不旋转

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -134,7 +134,7 @@ int main(void)
   __HAL_UART_ENABLE_IT(&huart3, UART_IT_IDLE);
   HAL_UART_Receive_DMA(&huart3, (uint8_t *)DBUS_V_UNION.GET_DATA, sizeof(DBUS_V_UNION.GET_DATA));
 
- printf("ok\r\n");  // huart1 
+  printf("ok\r\n");  // huart1 
 
   
   HAL_TIM_Base_Start_IT(&htim2);
